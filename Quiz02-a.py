c3f67e4a1daf6bbef1006fa23d02c0225ad6ad0c
ByronLarson <40004728+ByronLarson@users.noreply.github.com>
2018-06-20 20:54:57 -0500

    Add files via upload

---
+++ Quiz02-a.py
@@ -0,0 +1,13 @@
+def dot(vector01,vector02):
+'''
+This function compares the lengths of two vectors together.  If they are the same length it multiolies the first column of vector01 by the first column of vector02.  next it takes this value and stores it in the out container.  it repeats this multiplication for each column and adds the products together inside of the out container.
+'''
+
+  if len(vector01)==len(vector02) and len(vector01)!=0:#checks to see that the two vectors are the same length and the second vector is not 0
+    out = 0 # starts the holding array at 0
+    for k in range(len(v1)):
+        out += v1[k] * v2[k]# for each iteration after the product is done it adds the number into the container out
+    return out
+  else:
+    print"input invalid"
+    
