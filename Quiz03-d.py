5a1be269a58ddc3b6d9117c500f1d5d11aa2620e
ByronLarson <40004728+ByronLarson@users.noreply.github.com>
2018-06-22 11:23:24 -0500

    Update Quiz03-d.py

--- Quiz03-d.py
+++ Quiz03-d.py
@@ -1,6 +1,12 @@
 def infNorm(vector):
 
   '''
+  Input
+  vector should be 1 x n in length
+  
+  Output
+  largest should be a scalar
+  
   This function calculates the infinity norm of a vector.  The infinity norm is the maximum absolute value of its components.  I used a for loop to take the absolute value of every element in vector, then another for loop to compare each element in the newvector in order to find the largest value.
   '''
   newvector = []
