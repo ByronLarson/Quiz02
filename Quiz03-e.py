6d44bf0e816f0c25c735e4d8d419fd35a60ef721
ByronLarson <40004728+ByronLarson@users.noreply.github.com>
2018-06-22 10:40:15 -0500

    Update Quiz03-e.py

--- Quiz03-e.py
+++ Quiz03-e.py
@@ -1,4 +1,4 @@
-def infNorm(vector):
+def normalize(vector):
 
   '''
   This function calculates the normalization of a vector with respect to the infinity norm.  The infinity norm is the maximum absolute value of its components.  I used a for loop to take the absolute value of every element in vector, then another for loop to compare each element in the newvector in order to find the largest value.  From here we multiply 1/ largest times the original vector to get the normalized vector
@@ -21,5 +21,5 @@ def infNorm(vector):
 
   print(newestvector)
       
-testvec = [1,2,3,-555]
-print(infNorm(testvec)) 
+#testvec = [1,2,3,-555]
+#print(normalize(testvec)) 
