732d9591ce66838bc409bde5be33963ad658bde4
ByronLarson <40004728+ByronLarson@users.noreply.github.com>
2018-06-22 11:14:33 -0500

    Update Quiz03-a.py

--- Quiz03-a.py
+++ Quiz03-a.py
@@ -1,7 +1,13 @@
 def dot(vector01,vector02):
   '''
-  This function compares the lengths of two vectors together.  If they are the same length it multiolies the first column of vector01 by the first column of vector02.  next it takes this value and stores it in the out container.  it repeats this multiplication for each column and adds the products together inside of the out container.
-  '''
+  Inputs
+    vector01 should be 1 x n in length
+    vector02 should be 1 x n in length
+
+  Output should be a scalar
+
+  This function compares the lengths of two vectors together.  If they are the same length it multiolies the first column of vector01 by the first column of vector02.  Next it takes this value and stores it in the out container.  it repeats this multiplication for each column and adds the products together inside of the out container.
+    '''
 
   if len(vector01)==len(vector02) and len(vector01)!=0:#checks to see that the two vectors are the same length and the second vector is not 0
     out = 0 # starts the holding array at 0
@@ -11,3 +17,7 @@ def dot(vector01,vector02):
   else:
     print("input invalid")
     
+#testvec1 = a
+#testvec2 = [4,5,6]
+
+#print(dot(testvec1,testvec2))
