7d2ef86269ccb6a7a717ac4bdcd79421588f2a5c
ByronLarson <40004728+ByronLarson@users.noreply.github.com>
2018-06-22 11:21:57 -0500

    Update Quiz03-c.py

--- Quiz03-c.py
+++ Quiz03-c.py
@@ -1,10 +1,22 @@
 def scalarVecMulti(scalar,vector):
 
   '''
+  Input
+  scalar should be a number
+  vector should be a 1 x n in length
+  
+  Output
+  newvector should be a 1 x n in length
 
   This function inputs a scalar and a vector, then multiplies the scalar to every element in the vector for the length of the vector.
 
   '''
+  
+  cV = len(vector[0])
+  if cV > 1
+    print("Invalid Input")
+    return
+  
   newvector = []
 
   for i in range(len(vector)):
