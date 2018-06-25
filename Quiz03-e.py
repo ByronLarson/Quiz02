c9e62d800459100f6b0be642d7bcb34f75f6b6ce
ByronLarson <40004728+ByronLarson@users.noreply.github.com>
2018-06-25 14:40:05 -0500

    Update Quiz03-e.py

--- Quiz03-e.py
+++ Quiz03-e.py
@@ -7,12 +7,12 @@ def normalize(vector):
   Output
   newestvector is a 1 x n in length
   
-  This function calculates the normalization of a vector with respect to the infinity norm.  The infinity norm is the maximum absolute value of its components.  I used a for loop to take the absolute value of every element in vector, then another for loop to compare each element in the newvector in order to find the largest value.  From here we multiply 1/ largest times the original vector to get the normalized vector
+  This function calculates the normalization of a vector with respect to the infinity norm. The infinity norm is the maximum absolute value of its components.  I used a for loop to take the absolute value of every element in vector, then another for loop to compare each element in the newvector in order to find the largest value.  From here we multiply 1/ largest times the original vector to get the normalized vector
+  
+  I used the code from part D to find the largest absolute number in the vector and then just added another for loop to apply the 1/ largest to the original vector.
+
   '''
-  cV = len(vector[0])# tests that the vector is not a matrix
-  if cV > 1
-    print("Invalid Input")
-    return
+  
   
   newvector = []
 
@@ -30,7 +30,7 @@ def normalize(vector):
   for j in range(len(vector)):#for each iteration multiply 1/largest times each element in vector
     newestvector.append(1/largest * vector[j])
 
-  print(newestvector)
+  return newestvector
       
-#testvec = [1,2,3,-555]
+#testvec = [1,2,3,-55]
 #print(normalize(testvec)) 
